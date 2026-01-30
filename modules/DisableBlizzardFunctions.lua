6032f5635423720a4acffad91583dfb8947b22c7
Stormseer <denizakak@hotmail.com>
2026-01-30 12:15:41 +0100

    Attempted to remove specific duration texts on the Blizzard Buff Tracker, but didn't manage. This is just the commented out version of one that hits all buffs.

--- modules/DisableBlizzardFunctions.lua
+++ modules/DisableBlizzardFunctions.lua
@@ -50,3 +50,40 @@ f:SetScript("OnEvent", function()
     f:UnregisterAllEvents()
     f:SetScript("OnEvent", nil)
 end)
+
+--[[ Hides the cooldown text on all of the buff frames in the cooldown manager. 
+local function HideCooldownText(cooldownFrame)
+  if not cooldownFrame then return end
+  
+  -- Hide countdown numbers
+  cooldownFrame:SetHideCountdownNumbers(true)
+  cooldownFrame:SetDrawEdge(false)
+  
+  -- Hide any FontStrings in the cooldown
+  for _, region in ipairs({ cooldownFrame:GetRegions() }) do
+    if region:GetObjectType() == "FontString" then
+      region:Hide()
+      region:SetAlpha(0)
+    end
+  end
+end
+
+EventUtil.RegisterOnceFrameEventAndCallback("PLAYER_ENTERING_WORLD", function()
+  
+  -- Hook the SetCooldown function which is called when cooldowns are updated
+  hooksecurefunc(getmetatable(CreateFrame("Cooldown")).__index, "SetCooldown", function(self)
+    -- Check if this cooldown belongs to BuffIconCooldownViewer
+    local parent = self:GetParent()
+    if parent and parent:GetParent() == BuffIconCooldownViewer then
+      HideCooldownText(self)
+    end
+  end)
+  
+  -- Also process existing ones
+  for _, CdFrame in ipairs({ BuffIconCooldownViewer:GetChildren() }) do
+    if CdFrame.Cooldown then
+      HideCooldownText(CdFrame.Cooldown)
+    end
+  end
+end)
+--]]
